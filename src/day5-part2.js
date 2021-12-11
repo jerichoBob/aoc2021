116ec7e92fb80ef1f4262a80927404ae4b5539f2
Bob Seaton <robert.w.seaton.jr@gmail.com>
2021-12-11 01:08:59 -0500

    solved day 5 pat 2 (on the first try :)

--- src/day5-part2.js
+++ src/day5-part2.js
@@ -1,5 +1,5 @@
 /**
- * day5-part1.js
+ * day5-part2.js
  * Language: javascript
  * Path: src/day5-part1.js
  * Description:
@@ -7,13 +7,14 @@
  **/
 import fs from 'graceful-fs';
 const re_to_from = /^(\d+)\,(\d+)\s*\-\>\s*(\d+)\,(\d+)$/;
-let nondiag_lines = [];
+let line_array = [];
+// let diag_lines = [];
 class Line {
 	constructor(x1, y1, x2, y2) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
+		this.x1 = parseInt(x1);
+		this.y1 = parseInt(y1);
+		this.x2 = parseInt(x2);
+		this.y2 = parseInt(y2);
 	}
 	print = (str) => {
 		return `${str} ${this.x1},${this.y1} - ${this.x2},${this.y2}`;
@@ -29,21 +30,15 @@ const parseFile = (file) => {
 			const c = file_line.match(re_to_from);
 			// console.log(`coords: [${c[1]},${c[2]}]<---->[${c[3]},${c[4]}]`);
 			const line = new Line(c[1], c[2], c[3], c[4]);
-			if (c[1]===c[3]||c[2]===c[4]) {
 
-				nondiag_lines.push(line);
-				// console.log(`${line.print("|- >>>>>> non-diag line:")}`);
-			// } else {
-			// 	diag_lines.push(new Line(c[1],c[2],c[3],c[4]));
-			// 	console.log(`${line.print("   \\/ >>>>>> diag line:")}`);
-
-			}
+			line_array.push(line);
+			// console.log(`${line.print("line: ")}`);
 		}
 	});
 }
 
-const day5_part1 = () => {
-	console.log("day5 part1 exercise");
+const day5_part2 = () => {
+	console.log("day5 part2 exercise");
 	var base = process.env.PWD;
 	let file;
 	const test = false;
@@ -53,44 +48,36 @@ const day5_part1 = () => {
 		file = fs.readFileSync(base + '/src/day5-input.txt', 'utf8');
 	}
 	parseFile(file);
-
-	// find max x,y
-	let max_x = 0;
-	let max_y = 0;
-	nondiag_lines.forEach(line => {
-		max_x = Math.max(max_x, line.x1, line.x2);
-		max_y = Math.max(max_y, line.y1, line.y2);
-	});
-	console.log(`max_x: ${max_x} max_y: ${max_y}`);
-	console.log(`nondiag_lines: ${nondiag_lines.length}`);
-
+// 
 	let counter = {};
 	let ckey = "";
-	nondiag_lines.forEach(line => {
-		if (line.x1 === line.x2) {
-			const start = Math.min(line.y1, line.y2);
-			const end = Math.max(line.y1, line.y2);
-			for (let y = start; y <= end; y++) {
-				ckey = `${line.x1},${y}`;
-				counter[ckey]=(counter[ckey]||0)+1;
-			}
-			
-		} else { // line.y1 === line.y2
-			const start = Math.min(line.x1, line.x2);
-			const end = Math.max(line.x1, line.x2);
-			for (let x = start; x <= end; x++) {
-				ckey = `${x},${line.y1}`;
-				counter[ckey]=(counter[ckey]||0)+1;
-			}
+	line_array.forEach(line => {
+
+		const xsteps = Math.abs(line.x2 - line.x1);
+		const xincr = (line.x1 < line.x2) ? 1 : (line.x1 > line.x2) ? -1 : 0;
+		const ysteps = Math.abs(line.y2 - line.y1);
+		const yincr = (line.y1 < line.y2) ? 1 : (line.y1 > line.y2) ? -1 : 0;
+
+		// console.log(`${line.print("line: ")}`);
+		// console.log(`xsteps: ${xsteps}\txincr: ${xincr}\tysteps: ${ysteps}\tyincr: ${yincr}`);
+
+		for (let step=0;step<=Math.max(xsteps,ysteps);step++) {
+			let x = line.x1 + (step * xincr);
+			let y = line.y1 + (step * yincr);
+			ckey = `${x},${y}`;
+			counter[ckey]=(counter[ckey]||0)+1;
+			// console.log(`${ckey} :::: ${counter[ckey]}`);
 		}
 	});
 
+
 	let count = 0;
 	Object.keys(counter).filter(key => counter[key]>1).forEach(key => {
+		console.log(`${key} : ${counter[key]}`);
 		count++;
 	});
 	console.log(`count: ${count}`);
 }  
 
-export default day5_part1;
+export default day5_part2;
 
